effe30bafddfa89727a56f2028b4396859e8d211
phihash <insharp0220@gmail.com>
2025-11-30 19:45:46 +0900

    整理

---
+++ jouhou/Enrty/RootView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct RootView: View {
+    @State private var screenManager = ScreenManager()
+    @State private var contentManager = ContentManager()
+    
+    var body: some View {
+        VStack{
+            switch screenManager.currentScreen {
+            case .firstQuestion:
+                ContentView(screenManager: screenManager, contentManager: contentManager)
+            case .setSelection:
+                SetSelectionView(screenManager: screenManager, contentManager: contentManager)
+            }
+        }
+    }
+}
